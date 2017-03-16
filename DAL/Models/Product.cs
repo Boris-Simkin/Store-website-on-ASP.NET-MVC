5f983bd27acb210c59b9a03021c9a64edca2b80f
Boris <boria83@gmail.com>
2017-03-16 17:09:51 +0200

    Start project

---
+++ DAL/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public ICollection<User> Owners { get; set; }
+        public ICollection<User> Users { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+    }
+}
